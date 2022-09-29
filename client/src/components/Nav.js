c15e6c10c64edb14fc14c9ffa97aec7865ed46f1
Mike <ampgzo2@gmail.com>
2022-09-29 12:02:19 -0400

    create taskscontainer page under taskscontainer component

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -3,7 +3,7 @@ import React from "react";
 const Nav = () => {
     return (
         <nav className="navbar">
-            <h3>Team's todo list</h3>
+            <h3>Team's to do list</h3>
         </nav>
     );
 };
